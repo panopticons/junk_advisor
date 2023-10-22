b5fcd22ff8c1bd93d94784f2db1ce46f0828d896
“maria <thepanopticons@gmail.com>
2023-10-21 23:28:48 -0700

    new setup

--- pages/scan.js
+++ pages/scan.js
@@ -5,14 +5,22 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Scan</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1 className={styles.title}>
-          Learn <a href="/scan">Next.js!</a>
+            scan
         </h1>
+
+        <div className={styles.grid}>
+          <a href="https://nextjs.org/docs" className={styles.card}>
+            <h3>Documentation &rarr;</h3>
+            <p>Find in-depth information about Next.js features and API.</p>
+          </a>
+
+        </div>
       </main>
 
       <footer>
